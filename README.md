b33c81058c93d970e837d052ae05f4b99e3707eb
Leandro Fernandes <leo_fernandes@hotmail.com>
2018-03-02 15:42:45 -0300

    Modelagem generalizada do espaço de estados.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Uma implementaÃ§Ã£o Java dos mÃ©todos de busca cega em EspaÃ§os de Estados mais comuns nos cursos de InteligÃªncia Artificial.
+# Espaço de Estados e formas de Buscas
+
+Este projeto é um exemplo de implementação em Java dos métodos de busca cega mais comuns da área de Inteligência Artificial, que permitem resolver problemas diversos desde que modelados na forma de um Espaços de Estados.
+A solução do problema é dada através exploração do espaço, transitando entre diferentes estados possíveis até que seja alcançado um estado que atenda os objetivos estabelecidos para a busca (estado meta).
+
+## Problemas modelados:
+
+Dado o caráter generalista que as buscas oferecem, escolhemos dois problemas diferentes e os modelamos como um espaço de estados de maneira que seja possível demonstrar essas técnicas em funcionamento.
+
+- **Puzzle-8**
+
+Um quebra-cabeças combinatório e de deslizamento (do tipo _sliding tile puzzle_), composto por oito peças numeradas que são distribuídas no tabuleiro e organizadas em três linhas e três colunas.
+Dentre as nove posições do arranjo há sempre uma que encontra-se vazia. É possível deslizar qualquer peça adjacente (tanto horizontalmente quanto verticalmente) para a posição livre, conferindo ao tabuleiro uma nova distribuição.
+As peças não podem ser retiradas do tabuleiro, tampouco podem ser movimentadas diagonalmente ou sobre uma outra peça. Vale ressaltar que nem sempre qualquer seqüência que o usuário possa imaginar seria possível efetivamente, uma vez que  na realidade o processo de embaralhamento é feito usando as mesmas regras. Um referencial teórico sobre como é possível determinar se uma configuração do tabuleiro é válidas ou não, pode ser encontradas [aqui] (http://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html) 
+
+- **Jogo da Velha (Tic-Tac-Toe)**
+
+Consiste num jogo em que um quadriculado com nove posições, organizadas em três linhas e três colunas, é preenchido e no qual dois jogadores se confrontam fazendo suas jogadas alternadamente. Cada um deles faz a sua jogada marcando uma posição vazia com o seu símbolo.
+Jogada após jogada o tabuleiro vai sendo preenchido com os símbolos `X` e `O`. O ganhador da disputa será aquele que conseguir formar primeiro uma trinca de suas marcas numa disposição linear.
---
+++ src/espacoDeEstados/Estado.java
@@ -0,0 +1,106 @@
+package espacoDeEstados;
+
+import java.util.List;
+
+/**
+ * Esta classe implementa o nível mais abstrato de representação do problema sob
+ * a forma de um espaço de estados. Pontualmente, modela uma possível situação ou
+ * configuração deste problema (estado), além de ser possível vinculá-lo com outros
+ * estados, que é o quesito fundamental para construir espaço de estados.
+ * 
+ * @author Leandro Fernandes
+ * 
+ * @param <T> tipo utilizado para representar e armazenar uma descrição do estado,
+ *            segundo o problema que estiver sendo modelado. Trata-se de um tipo
+ *            abstrato de dados (TAD), que permite a esta classe operar com qualquer
+ *            forma de descrição que for adotada e considerada mais adequada a
+ *            representação do problema em questão.
+ */
+public abstract class Estado<T> {
+	
+	private int id = 0;					// identificador do estado
+	private int nivel;					// nível em que o estado se encontra na árvore de busca
+	private Estado<?> ancestral;		// referencia ao nó pai na árvore de busca
+	
+	/**
+	 * Define o número identificador do estado.
+	 * @param identificador do estado
+	 */
+	public int setId(int id) {
+		return this.id = id;
+	}
+	
+	/**
+	 * Recupera o identificador do estado.
+	 * @return identificador do estado
+	 */
+	public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Define o nível que o estado (nodo) ocupa na árvore de busca.
+	 * @param nível do estado na árvore de busca
+	 */
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+	
+	/**
+	 * Método de acesso que permite recuperar qual o nível (profundidade em
+	 * relação a raiz) que este estado ocupa na árvore de busca.
+	 * @return nível do nodo na árvore de busca
+	 */
+	public int getNivel() {
+		return nivel;
+	}
+	
+	/**
+	 * Define qual o estado que originou o estado corrente dentro do espaço
+	 * de busca, ou seja, qual é o seu estado predecessor.
+	 * @param referência ao estado ancestral
+	 */
+	public void setAncestral(Estado<?> estado) {
+		ancestral = estado;
+	}
+	
+	/**
+	 * Recupera o estado predecessor deste estado no espaço de busca.
+	 * @return referência ao estado anterior, sob o qual a ação resultou no estado atual
+	 */
+	public Estado<?> getAncestral() {
+		return ancestral;
+	}
+	
+	/**
+	 * Status, descrição ou configuração representativa para o estado.
+	 * @param informações que caracterizam este estado 
+	 */
+	public abstract void setEstado(T cfg);
+	
+	/**
+	 * Retorna a configuração do estado 
+	 * @return todas as informações que caracterizam este estado
+	 */
+	public abstract T getEstado();
+		
+	/**
+	 * Função que gera os estados sucessores de acordo com o problema
+	 * @return lista de estados sucessores (adjacentes) a partir deste estado
+	 */
+	public abstract List<?> getSucessores();
+	
+	/**
+	 * Permite verificar se este estado é igual a outro.
+	 * @param o estado qual se deseja comparar com este
+	 * @return true ou false
+	 */
+	public abstract boolean equals(Object estado);
+	
+	/**
+	 * Retorna uma representação do estado numa forma textual e que possa ser
+	 * apresentada na console, por exemplo.
+	 * @return uma String representativa contendo as informações descritivas do estado
+	 */
+	public abstract String toString();
+}
