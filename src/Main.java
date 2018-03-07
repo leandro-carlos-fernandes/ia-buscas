a615777a9c12d3606a5ba7b93211fecf224b4dbc
Leandro Fernandes <leo_fernandes@hotmail.com>
2018-03-07 14:19:42 -0300

    Teste de busca usando o quebra-cabeças Puzzle-8

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import espacoDeEstados.Estado;
 import espacoDeEstados.Puzzle8;
-import estrategiasDeBusca.BuscaCega;
-import estrategiasDeBusca.BuscaEmLargura;
+import estrategiasDeBusca.cega.BuscaCega;
+import estrategiasDeBusca.cega.BuscaEmLargura;
 
 public class Main {
 
 	@SuppressWarnings("rawtypes")
 	public static void main(String[] args) {
 		
-		char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
+		//char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
+		char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
+		//char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
+		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
 		Estado puzzleInicial = new Puzzle8(cfgIni);
 
 		char[] cfgMeta = {'1','2','3','4','5','6','7','8',' '};
@@ -21,7 +24,7 @@ public class Main {
 		for(Estado e : busca.getCaminhoSolucao()) {
 			System.out.println(e);
 		}
-		
+
 		System.exit(0);
 	}
 
